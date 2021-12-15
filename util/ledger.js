8712e6c561a0cc15086119bbda6413a1c03546e9
romefeller <alexgrcol@hotmail.com>
2021-12-15 11:00:27 -0300

    Fixing exposed modules

--- util/ledger.js
+++ util/ledger.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.window = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict'
 
@@ -2298,6 +2298,13 @@ const SpeculosTransport = require("@ledgerhq/hw-transport-node-speculos").defaul
 const TranspWeb = require("@ledgerhq/hw-transport-webhid").default;
 const WebUSB = require("@ledgerhq/hw-transport-webusb").default;
 
+function load_modules(){
+    window.Kadena = Kadena;
+    window.SpeculosTransport = SpeculosTransport;  
+    window.TranspWeb = TranspWeb;
+    window.WebUSB = WebUSB;
+}
+
 async function main() {
   const apduPort = 8000;
   const transport = await SpeculosTransport.open({ apduPort });
@@ -2317,6 +2324,7 @@ async function main() {
   process.exit()
 }
 
+load_modules();
 //main().catch(e => console.error(e));
 
 }).call(this)}).call(this,require('_process'))
@@ -14833,5 +14841,4 @@ process.chdir = function (dir) {
 };
 process.umask = function() { return 0; };
 
-},{}]},{},[2])(2)
-});
+},{}]},{},[2]);
